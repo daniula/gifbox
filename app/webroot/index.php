7c2dd672975a31080c66582dfbc503f3526f6018
Daniel Mendalka <daniel.mendalka@gmail.com>
2012-11-10 23:08:18 +0200

    rollback to deafult CAKE_CORE_INCLUDE_PATH path

--- app/webroot/index.php
+++ app/webroot/index.php
@@ -57,7 +57,7 @@ if (!defined('APP_DIR')) {
  * Leaving this constant undefined will result in it being defined in Cake/bootstrap.php
  */
 
-	define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . 'cakephp');
+	// define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . 'cakephp');
 
 /**
  * Editing below this line should NOT be necessary.
